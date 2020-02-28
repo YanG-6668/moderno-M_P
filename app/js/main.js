810a148e5c683333d1837afb5f5a241d9f481c8c
YanG-6668 <goch.yannn@gmail.com>
2020-02-28 15:48:43 +0200

    added upload page/ withdrawals page

--- app/js/main.js
+++ app/js/main.js
@@ -45,6 +45,8 @@ $(function() {
         return false;
     });
 
+    $('input[type="file"], select').styler();
+
     $('.menu__btn').on('click', function() {
         $('.menu__list').slideToggle();
     });
